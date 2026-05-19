fca21075fd4756405b2e6451c7915f38748faa10
GERSONTORRES87 <gersontorres87@yahoo.com.co>
2026-05-18 19:49:39 -0500

    Revertido login - app abierta para todos

--- mascotas/urls.py
+++ mascotas/urls.py
@@ -1,15 +1,19 @@
 from django.urls import path
 from . import views
 
-app_name = 'mascotas'
+app_name = "mascotas"
 
 urlpatterns = [
-    path('', views.inicio, name='inicio'),
-    path('mascotas/', views.MascotaListView.as_view(), name='lista'),
-    path('mascotas/<int:pk>/', views.MascotaDetailView.as_view(), name='detalle'),
-    path('mascotas/nueva/', views.MascotaCreateView.as_view(), name='crear'),
-    path('mascotas/<int:pk>/editar/', views.MascotaUpdateView.as_view(), name='editar'),
-    path('mascotas/<int:pk>/eliminar/', views.MascotaDeleteView.as_view(), name='eliminar'),
-    path('mascotas/<int:pk>/vacuna/', views.agregar_vacuna, name='agregar_vacuna'),
-    path('vacuna/<int:pk>/eliminar/', views.eliminar_vacuna, name='eliminar_vacuna'),
+    path("", views.inicio, name="inicio"),
+    path("mascotas/", views.MascotaListView.as_view(), name="lista"),
+    path("mascotas/<int:pk>/", views.MascotaDetailView.as_view(), name="detalle"),
+    path("mascotas/nueva/", views.MascotaCreateView.as_view(), name="crear"),
+    path("mascotas/<int:pk>/editar/", views.MascotaUpdateView.as_view(), name="editar"),
+    path(
+        "mascotas/<int:pk>/eliminar/",
+        views.MascotaDeleteView.as_view(),
+        name="eliminar",
+    ),
+    path("mascotas/<int:pk>/vacuna/", views.agregar_vacuna, name="agregar_vacuna"),
+    path("vacuna/<int:pk>/eliminar/", views.eliminar_vacuna, name="eliminar_vacuna"),
 ]
